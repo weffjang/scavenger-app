aa3fb912dede83c5a8c88799efd86dc5b44a5e8f
jwang <Wanjey@bitbucket.org>
2022-12-21 22:52:31 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [inputValue, setInputValue] = useState('');
+
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'Paris', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What is 1+1?',
+			answerOptions: [
+				{ answerText: '2', isCorrect: true },
+				{ answerText: 'two', isCorrect: true }
+			],
+		},
+		{
+			questionText: 'What is Peter? Answer is yes',
+			answerOptions: [
+				{ answerText: 'yes', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	const handleAnswerButtonClick = event => {
+		const nextQuestion = currentQuestion + 1;
+
+		for (let i = 0; i < questions[currentQuestion].answerOptions.length; i++) {
+			if (inputValue === questions[currentQuestion].answerOptions[i].answerText) {
+				setInputValue('');
+				if (nextQuestion < questions.length) {
+					setCurrentQuestion(nextQuestion);
+				}
+			}
+		}
+		event.preventDefault();
+	}
+
+	const handleChange = event => {
+		setInputValue(event.target.value);
+	}
+
+	return (
+		<div className='app'>
+			<div className='question-section'>
+				<div className='question-count'>
+					<span>Clue {currentQuestion + 1}</span>/{questions.length}
+				</div>
+				<div className='question-text'>{questions[currentQuestion].questionText}</div>
+			</div>
+			<div className='answer-section'>
+				<form onSubmit={handleAnswerButtonClick}>
+					<input type="text" value={inputValue} onChange={handleChange} />
+					<input type="submit" value={'Submit'} className = "submitBtn"/>
+				</form>
+			</div>
+		</div>
+	);
 }
-
-export default App;
